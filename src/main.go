cab3c50e8e42cef5f3d53d23b1907552a0e64fdb
Ivan Dujaut <dujautivan@gmail.com>
2023-03-26 16:19:27 -0300

    [NEW] - Declaracion de variables

--- src/main.go
+++ src/main.go
@@ -4,6 +4,31 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hello world!")
+	// Declaracion de constantes
+	const pi float64 = 3.14
+	const pi2 = 3.1415
 
+	fmt.Println("pi", pi)
+	fmt.Println("pi2", pi2)
+
+	//Declaracion de variables enteras
+	base := 12 //esos : indica que esta variable no ha sido declarada anterioremnte y una vez que := va a crear esa varable Y le asigna ese valor parseando el tipo de dato
+	var altura int = 14 //indico que es una variable, la nombre, le indico el tipo de dato y le asigno el valor. Es explicito
+	var area int // todo lo anterior co la diferencia de que no asigo el valor
+
+	fmt.Println(base, altura, area)
+
+	// Zero values: valor que va a tener por defecto al momento de 
+	// crear una variable y no le asignamos un valor 
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	// Area de un cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("Area cuadrado: ", areaCuadrado)
 }
